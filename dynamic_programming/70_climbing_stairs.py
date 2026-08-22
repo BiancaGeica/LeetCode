11c2439190a5284f1cfc62afdc6bc9c0776dcb66
Elena-Bianca Geica <geicaelenabianca@gmail.com>
2026-08-22 16:37:32 +0300

    Solve climbing_stairs and add gitignore

---
+++ dynamic_programming/70_climbing_stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        arr = []
+        for i in range(n + 1):
+            if i <= 1:
+                arr.append(1)
+            else:
+                arr.append(0)
+
+        for i in range(2, n + 1):
+            arr[i] = arr[i-1] + arr[i-2]
+
+        return arr[n]
+
+# Time complexity = theta(n)
